1e226b0fd6e0c002dc1d1f5c7662b2e9365bb704
Obsydion <slaterobsydion@gmail.com>
2024-01-27 14:36:16 -0700

    Added JSX for the page template

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ const Navbar = () => {
     </nav>
   );
 }
+const Sidebar = () => {
+  return (
+    <div className="d-none d-md-block col-md-3">
+      <div className="border border-primary py-4 px-3">
+        Sidebar
+      </div>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div className="border-top p-2">
+      Template Demo © 2019
+    </div>
+  )
+}
+const Template = (props) => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container py-4">
+        <div className="row">
+          <div className="col-12 col-md-9">
+            {props.children}
+          </div>
+          <Sidebar />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <Template>
+      <h1>Main content</h1>
+    </Template>
+  );
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
